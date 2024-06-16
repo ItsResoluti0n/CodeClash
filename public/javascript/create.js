dc936b08a5559591b3e81294bf846d9780eb859c
p51224dt <dylan.taylor@student.manchester.ac.uk>
2024-06-16 13:24:26 +0100

    create room server db connection

--- public/javascript/create.js
+++ public/javascript/create.js
@@ -1,3 +1,4 @@
+//makes the password field active/not active depending on checkbox status
 const passActivate = () => {
     const check = document.getElementById("private-check");
     const input = document.getElementById("password-input");
@@ -11,4 +12,27 @@ const passActivate = () => {
         input.classList.remove("input-disabled")
         label.classList.remove("label-disabled")
     }
+}
+
+//creates a room based on the given inputs
+const createRoom = () => {
+    const name = document.getElementById("room-input").value;
+    const password = document.getElementById("password-input").value;
+    const private = document.getElementById("private-check").checked;
+    if(!private) {
+        password = null
+    }
+    //data to be sent to server
+    const data = {
+        roomName: name,
+        password: password
+    };
+    fetch("http://localhost:3000/room-create", {    //Webpage URL
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        //The data being sent converted from JavaScript value to JSON format
+        body: JSON.stringify(data)
+    }).then(window.location.href = "lobby.html")
 }
--- server.js
+++ server.js
@@ -3,12 +3,43 @@ const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
 const { v4: uuidV4 } = require("uuid")
+const { MongoClient } = require("mongodb");
+const bodyParser = require('body-parser')
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+app.use(express.json({limit: "200mb"}));
 
+//initial page to display
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 server.listen(3000)
+
+//connect to mongodb database
+const uri =
+  "mongodb+srv://dylan:Digglet_1805@cluster0.17o94mu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+//get room data from room-create address
+app.post('/room-create', function(req, res) {
+    const formData = req.body;
+    CreateRoom(formData.roomName, formData.password);
+});
+
+//function to create room, will send the room details to database
+async function CreateRoom(name, password) {
+  try {
+    await client.connect();
+    const db = client.db("codeclash");
+    const coll = db.collection("rooms");
+    const docs = [
+        {roomName: name, password: password}
+      ];
+    const result = await coll.insertMany(docs);
+  } finally {
+    await client.close();
+  }
+}
+
